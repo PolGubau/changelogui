b4b21fc33127cb28cb993d8f0ce8a2ea7323eab2
Pol Gubau Amores <gubaupol@gmail.com>
2025-01-12 13:42:05 +0100

    feat: ✨ add new static template

---
+++ release.config.cjs
@@ -0,0 +1,9 @@
+const {
+	createMonolithicReleaseConfig,
+} = require("@polgubau/semantic-release-config");
+
+const name = "@pol-gubau/changelogui";
+
+module.exports = createMonolithicReleaseConfig({
+	name,
+});
